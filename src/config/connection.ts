82c8e8a9b9b80669a6e0924123cbc846fcfe1180
raihanpf22 <raihanpambagyo@.com>
2022-12-29 11:46:26 +0700

    update bismillah bisa

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -29,9 +29,9 @@ const salt = bcrypt.genSaltSync(10);
 const hash = bcrypt.hashSync("P@ssw0rd", salt);
 
 const user = new User({
-  name: "Bob Sadino",
-  email: "bobsadino@gmail.com",
-  role: "Super Admin",
+  name: "Bob Sadino" as string,
+  email: "bobsadino@gmail.com" as string,
+  role: "Super Admin" as string,
   password: hash,
   createdAt: new Date(),
   updatedAt: new Date(),
@@ -39,21 +39,21 @@ const user = new User({
 user.save();
 
 // Seeder For Data Car
-// const car = new Car({
-//   no_police: "B 4022 KZB",
-//   brand: "SUZUKI",
-//   model: "LMPV",
-//   image:"http://res.cloudinary.com/dgswqbhcm/image/upload/v1672200109/fdqfv4cggfkmhdsimigu.jpg",
-//   price_perday:1500000,
-//   capacity:7,
-//   status:true,
-//   transmision:"Manual",
-//   type:"Ertiga",
-//   createdBy:"bobsadino@gmail.com",
-//   updatedBy:"bobsadino@gmail.com",
-//   createdAt: new Date(),
-//   updatedAt: new Date(),
-// });
-// car.save();
+const car = new Car({
+  no_police: "B 4022 KZB",
+  brand: "SUZUKI",
+  model: "LMPV",
+  image:"http://res.cloudinary.com/dgswqbhcm/image/upload/v1672200109/fdqfv4cggfkmhdsimigu.jpg",
+  price_perday:1500000,
+  capacity:7,
+  status:true,
+  transmision:"Manual",
+  type:"Ertiga",
+  createdBy:"bobsadino@gmail.com",
+  updatedBy:"bobsadino@gmail.com",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+});
+car.save();
 
 export default connection;
